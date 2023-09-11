03fc7c18e0db5e73b45baf3ed398be54b4e18744
Mostafa Hosseini <kavrindev@gmail.com>
2023-09-11 23:08:21 +0330

    Finish Day 23 part 1

---
+++ Challenge1App-ShareTheFlag/ShareTheFlag/ShareTheFlag/ViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  ShareTheFlag
+//
+//  Created by Mostafa Hosseini on 9/11/23.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    
+    var flags = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        title = "Share The Flag"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        let path = Bundle.main.resourcePath!
+        let items = try! FileManager.default.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasSuffix(".png") {
+                flags.append(item)
+            }
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return flags.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Flag", for: indexPath)
+        var config = UIListContentConfiguration.cell()
+        config.text = "\(flags[indexPath.row].dropLast(7))"
+        config.image = UIImage(named: flags[indexPath.row])
+        config.imageProperties.maximumSize = CGSize(width: 64, height: 48)
+        cell.contentConfiguration = config
+        return cell
+    }
+    
+//    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
+//            vc.selectedImage = pictures[indexPath.row]
+//            navigationController?.pushViewController(vc, animated: true)
+//        }
+//    }
+
+}
+
