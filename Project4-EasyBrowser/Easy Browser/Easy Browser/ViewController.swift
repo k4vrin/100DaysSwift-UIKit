6ccf8f01bc3de4e15a23b0f5d27c595b743de90f
Mostafa Hosseini <kavrindev@gmail.com>
2023-09-23 17:15:56 +0330

    Finish Day 25

--- Project4-EasyBrowser/Easy Browser/Easy Browser/ViewController.swift
+++ Project4-EasyBrowser/Easy Browser/Easy Browser/ViewController.swift
@@ -10,6 +10,8 @@ import WebKit
 
 class ViewController: UIViewController, WKNavigationDelegate {
     var webView: WKWebView!
+    var progressView: UIProgressView!
+    var websites = ["www.apple.com", "www.hackingwithswift.com"]
 
     override func loadView() {
         webView = WKWebView()
@@ -20,29 +22,69 @@ class ViewController: UIViewController, WKNavigationDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
+        setupWebView()
+        setupToolbarAndNavBar()
+        // the context value will be sent back whenever the value is changed (for checking which observer)
+        webView.addObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), options: .new, context: nil)
+        
+    }
 
-        let url = URL(string: "https://www.hackingwithswift.com")!
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        self.title = webView.title
+    }
+    
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        let url = navigationAction.request.url
+        if let host = url?.host {
+            if websites.contains(host) {
+                decisionHandler(.allow)
+                return
+            }
+        }
+        decisionHandler(.cancel)
+        
+    }
+    
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == "estimatedProgress" {
+            progressView.progress = Float(webView.estimatedProgress)
+        }
+    }
+    
+    private func setupWebView() {
+        let url = URL(string: "https://\(websites[0])")!
         webView.load(URLRequest(url: url))
         webView.allowsBackForwardNavigationGestures = true
     }
-
+    
+    private func setupToolbarAndNavBar() {
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
+        let spacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webView.reload))
+        
+        progressView = UIProgressView(progressViewStyle: .default)
+        progressView.sizeToFit()
+        let progressButton = UIBarButtonItem(customView: progressView)
+        
+        toolbarItems = [progressButton, spacer, refresh]
+        navigationController?.isToolbarHidden = false
+    }
+    
     @objc private func openTapped() {
         let alertController = UIAlertController(title: "Open page...", message: nil, preferredStyle: .actionSheet)
-        alertController.addAction(UIAlertAction(title: "apple.com", style: .default, handler: openPage))
-        alertController.addAction(UIAlertAction(title: "hackingwithswift.com", style: .default, handler: openPage))
+        websites.forEach { website in
+            alertController.addAction(UIAlertAction(title: website, style: .default, handler: openPage))
+        }
         alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel))
         alertController.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
         present(alertController, animated: true)
     }
-
+    
     private func openPage(action: UIAlertAction) {
         guard let website = action.title else { return }
         guard let url = URL(string: "https://\(website)") else { return }
         webView.load(URLRequest(url: url))
     }
     
-    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
-        self.title = webView.title
-    }
 }
