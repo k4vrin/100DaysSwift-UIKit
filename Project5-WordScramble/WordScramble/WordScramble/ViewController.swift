b9d9c02ff965e253c974aa88f7eab1bfb8c05616
Mostafa Hosseini <kavrindev@gmail.com>
2023-11-23 10:59:03 +0330

    Finish Day 28

--- Project5-WordScramble/WordScramble/WordScramble/ViewController.swift
+++ Project5-WordScramble/WordScramble/WordScramble/ViewController.swift
@@ -11,8 +11,6 @@ class ViewController: UITableViewController {
     var allWords = [String]()
     var usedWords = [String]()
     
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -38,7 +36,6 @@ class ViewController: UITableViewController {
     }
     
     private func readStartFile() {
-        
         if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
             if let startWords = try? String(contentsOf: startWordsURL) {
                 allWords = startWords.components(separatedBy: "\n")
@@ -60,7 +57,7 @@ class ViewController: UITableViewController {
         let ac = UIAlertController(title: "Enter answer", message: nil, preferredStyle: .alert)
         ac.addTextField()
         let submitAction = UIAlertAction(title: "Submit", style: .default) { [weak self, weak ac] _ in
-            guard let answer = ac?.textFields?[0].text else {return}
+            guard let answer = ac?.textFields?[0].text else { return }
             self?.submit(answer)
         }
         
@@ -69,8 +66,53 @@ class ViewController: UITableViewController {
     }
 
     private func submit(_ answer: String) {
+        let lowerAnswer = answer.lowercased()
+        let errorTitle: String
+        let errorMessage: String
         
+        switch true {
+        case !isPossible(word: lowerAnswer):
+            errorTitle = "Word not possible"
+            errorMessage = "You can't spell that word from \(title!.lowercased())"
+        case !isOriginal(word: lowerAnswer):
+            errorTitle = "Word alredy used"
+            errorMessage = "Be more original!"
+        case !isReal(word: lowerAnswer):
+            errorTitle = "Word not recognized"
+            errorMessage = "You can't just make them up, you know!"
+        default:
+            usedWords.insert(answer, at: 0)
+            let indexPath = IndexPath(row: 0, section: 0)
+            tableView.insertRows(at: [indexPath], with: .automatic)
+            return
+        }
+        
+        let ac = UIAlertController(title: errorTitle, message: errorMessage, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
     }
-
+    
+    private func isPossible(word: String) -> Bool {
+        guard var tempWord = title?.lowercased() else { return false }
+        for letter in word {
+            if let position = tempWord.firstIndex(of: letter) {
+                tempWord.remove(at: position)
+            } else {
+                return false
+            }
+        }
+        
+        return true
+    }
+    
+    private func isOriginal(word: String) -> Bool {
+        return !usedWords.contains(word)
+    }
+    
+    private func isReal(word: String) -> Bool {
+        let chekcer = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = chekcer.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        return misspelledRange.location == NSNotFound
+    }
 }
-
