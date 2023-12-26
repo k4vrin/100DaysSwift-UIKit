9386cd66960f052ce66ea27fa57206aa067d54fa
Mostafa Hosseini <kavrindev@gmail.com>
2023-12-26 17:55:20 +0330

    Finish Day 31

--- Project6-AutoLayout/GuessTheFlag/AutoLayout/VisualFormatVC.swift
+++ Project6-AutoLayout/GuessTheFlag/AutoLayout/VisualFormatVC.swift
@@ -46,15 +46,34 @@ class VisualFormatVC: UIViewController {
         label5.sizeToFit()
         view.addSubview(label5)
         
-        let viewsDict = ["label1": label1, "label2": label2, "label3": label3, "label4": label4, "label5": label5]
+//        let viewsDict = ["label1": label1, "label2": label2, "label3": label3, "label4": label4, "label5": label5]
+//        
+//        for label in viewsDict.keys {
+//            // H -> Horizontal
+//            // || -> Edge of the parent view. in this case "view"
+//            // [] -> actual subview
+//            view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[\(label)]|", metrics: nil, views: viewsDict))
+//        }
+//        // - -> 10px space
+//        // (==72) -> height 88
+//        // || -> Start and End of screen.  A pipe symbol in Visual Formatting Language means the edge of the parent view.
+//
+//        let metrics = ["labelHeight": 88]
+//        
+//        // @999 -> constraint priority is a value between 1 & 1000. 999 means it's very important but not requiered
+//        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[label1(labelHeight@999)]-[label2(label1)]-[label3(label1)]-[label4(label1)]-[label5(label1)]-(>=10)-|", metrics: metrics, views: viewsDict))
         
-        for label in viewsDict.keys {
-            // H -> Horizontal
-            // || -> Edge of the parent view. in this case "view"
-            // [] -> actual subview
-            view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[\(label)]|", metrics: nil, views: viewsDict))
+        var previous: UILabel?
+        for label in [label1, label2, label3, label4, label5] {
+            label.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+            label.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+            label.heightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.heightAnchor, multiplier: 0.2, constant: -10).isActive = true
+            if let previous = previous {
+                label.topAnchor.constraint(equalTo: previous.bottomAnchor, constant: 10).isActive = true
+            } else {
+                label.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0).isActive = true
+            }
+            previous = label
         }
-        // - -> 10px space
-        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:[label1]-[label2]-[label3]-[label4]-[label5]", metrics: nil, views: viewsDict))
     }
 }
